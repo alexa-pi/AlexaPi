a3f9255ff598fd7cdd1b6c5e4b7705c8c3dfb4d9
Mason Stone <maso27@users.noreply.github.com>
2016-06-23 15:30:31 -0600

    Faster, constant always-on monitoring
    Intended to be run in the backgound.  It will restart main.py in "silent" mode if it gets interrupted.
    To stop the monitoring, enter the command "touch /tmp/dont_start"
    To restart the monitoring, "rm /tmp/dont_start"

--- monitorAlexa.sh
+++ monitorAlexa.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if pgrep python -a | grep main.py > /dev/null; then
-        echo "Alexa is already running."
-else
-        python /root/AlexaPi/main.py -s &
-fi
+while :
+do
+        if [ -f /tmp/dont_start ]; then
+                /etc/init.d/AlexaPi stop
+                sleep 3
+        elif ! pgrep python -a | grep main.py > /dev/null; then
+                /etc/init.d/AlexaPi silent
+                sleep 5
+        fi
+done
