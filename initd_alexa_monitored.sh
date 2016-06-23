6e134e8a4cd42764d8b0c9524e9d99ddee23000f
Mason Stone <maso27@users.noreply.github.com>
2016-06-23 15:27:57 -0600

    Always-on monitoring
    Also runs "monitorAlexa.sh" in the background.

---
+++ initd_alexa_monitored.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+### BEGIN INIT INFO
+# Provides:          AlexaPi
+# Required-Start:    $all
+# Required-Stop:     $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: AlexaPi Service
+# Description:       Start / Stop AlexaPi Service
+### END INIT INFO
+
+exec > /var/log/alexa.log 2>&1 
+case "$1" in
+
+start)
+    echo "Starting Alexa..."
+    python /root/AlexaPi/main.py &
+    /root/AlexaPi/monitorAlexa.sh &
+;;
+
+silent)
+    echo "Starting Alexa in silent mode..."
+    python /root/AlexaPi/main.py -s &
+;;
+
+stop)
+    echo "Stopping Alexa.."
+    pkill -f AlexaPi\/main\.py
+;;
+
+restart|force-reload)
+        echo "Restarting Alexa.."
+        $0 stop
+        sleep 2
+        $0 start
+        echo "Restarted."
+;;
+*)
+        echo "Usage: $0 {start|silent|stop|restart}"
+        exit 1
+esac
